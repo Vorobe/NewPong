3403bbaf320753c2195f72134e2b83f449f1bc91
Vorobe <vorobiov1992@gmail.com>
2019-08-11 22:07:16 +0300

    First Version

---
+++ Assets/Scenes/Paddle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    float height;
+
+    string input;
+
+    public bool isRight;
+
+   
+    void Start()
+    {
+        height = transform.localScale.y;
+        
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+        isRight = isRightPaddle;
+
+        Vector2 pos = Vector2.zero;
+       
+        if (isRightPaddle)
+        {
+            pos = new Vector2(Gamemanager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x;
+
+            input = "PaddleRight";
+        }
+        else
+        {
+            pos = new Vector2(Gamemanager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+
+            input = "PaddleLeft";
+        }
+
+        transform.position = pos;
+        transform.name = input;
+        
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        if (transform.position.y < Gamemanager.bottomLeft.y + height / 2 && move < 0)
+        {
+            move = 0;
+        }
+        if (transform.position.y > Gamemanager.topRight.y - height / 2 && move  > 0)
+        {
+            move = 0;
+        }
+
+
+        transform.Translate(move * Vector2.up);
+    }
+
+    
+}
